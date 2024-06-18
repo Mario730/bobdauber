75572834724f0850735d273ca67018ab8e3a61dc
Mario <mario55555@cox.net>
2024-06-18 14:28:43 -0600

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `bobdauber`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
