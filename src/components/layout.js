a07e22a1a0b0b255ad3ad0d09458c44c74fed196
Mario <mario55555@cox.net>
2024-07-08 15:22:57 -0700

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,15 +2,16 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import {
     container,
-    navLinks,
-    logo
+    navLinks
 } from './layout.module.css'
+
+import logo from "../images/icon-512x512.png"
 
 const Layout = ({ pageTitle, children }) => {
     return (
         <div className={container}>
             <nav>
-                <Link to="/" className={logo}>BOB<br/>DAUBER</Link>
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
                 <div className={navLinks}>
                     <Link to="/gallery">GALLERY</Link>
                     <Link to="/about">ABOUT</Link>
